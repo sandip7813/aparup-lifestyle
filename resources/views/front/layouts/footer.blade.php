86341b1ae6d404e234b4775b74ea4af9fff7d584
Sandip Nandy <sandeepnandi4@gmail.com>
2023-04-17 10:59:56 +0530

    Footer navigation

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,58 +1,39 @@
+@php
+$allCategories = categoriesWithDescendants();
+@endphp
 <!-- Footer Start-->
 <footer class="fixed-footer bg-dark text-white pt-100 pb-100 overflow-hidden">
     <div class="container-larger">
         <div class="row align-items-center">
             <div class="col-lg-4 col-md-12 text-center  d-none d-md-flex">
-                <img class="d-inline-block float-lg-left mb-30 mr-30" src="http://via.placeholder.com/105x203" alt="ads2">
+                {{-- <img class="d-inline-block float-lg-left mb-30 mr-30" src="http://via.placeholder.com/105x203" alt="ads2"> --}}
                 <div class="d-inline-block">
                     <div class="header-logo mb-20">
                         <h1 class="font-secondary"><a class="text-white" href="{{ config('app.url') }}">{{ config('app.name') }}</a></h1>
                         <p class="tagline font-primary">{{ config('app.site_caption') }}</p>
                     </div>
                     <p class="site-des">123 Main Street <br>New York, NY 10001</p>
-                    <div class="view-more text-center mt-30">
+                    {{-- <div class="view-more text-center mt-30">
                         <button class="btn btn-outline-white border-radius-0 font-weight-thin">Subcrible</button>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
             <div class="col-lg-8 col-md-12">
                 <div class="row">
-                    <div class="col">
-                        <h5 class="widget-title-muted mb-15">About</h5>
-                        <ul class="mb-30 mr-30 text-white">
-                            <li class="cat-item cat-item-2"><a class="text-white" href="category.html">Terms & Conditions</a></li>
-                            <li class="cat-item cat-item-3"><a class="text-white" href="category.html">Help & Support Policy</a></li>
-                            <li class="cat-item cat-item-4"><a class="text-white" href="category.html">Licensing Policy</a></li>
-                            <li class="cat-item cat-item-5"><a class="text-white" href="category.html">Refund Policy</a></li>
-                        </ul>
-                    </div>
-                    <div class="col">
-                        <h5 class="widget-title-muted mb-15">News</h5>
-                        <ul class="mb-30 mr-30 text-white">
-                            <li class="cat-item cat-item-2"><a class="text-white" href="category.html">Mindfulness</a></li>
-                            <li class="cat-item cat-item-3"><a class="text-white" href="category.html">Environment</a></li>
-                            <li class="cat-item cat-item-4"><a class="text-white" href="category.html">Religion</a></li>
-                            <li class="cat-item cat-item-5"><a class="text-white" href="category.html">Recipes</a></li>
-                        </ul>
-                    </div>
-                    <div class="col">
-                        <h5 class="widget-title-muted mb-15">Travel</h5>
-                        <ul class="mb-30 mr-30 text-white">
-                            <li class="cat-item cat-item-2"><a class="text-white" href="category.html">Destinations</a></li>
-                            <li class="cat-item cat-item-3"><a class="text-white" href="category.html">Travel Tips</a></li>
-                            <li class="cat-item cat-item-4"><a class="text-white" href="category.html">Hotels review</a></li>
-                            <li class="cat-item cat-item-5"><a class="text-white" href="category.html">Air ticket</a></li>
-                        </ul>
-                    </div>
-                    <div class="col">
-                        <h5 class="widget-title-muted mb-15">Healthy</h5>
-                        <ul class="mb-30 mr-30 text-white">
-                            <li class="cat-item cat-item-2"><a class="text-white" href="category.html">Integrative Health</a></li>
-                            <li class="cat-item cat-item-3"><a class="text-white" href="category.html">Mental Health</a></li>
-                            <li class="cat-item cat-item-4"><a class="text-white" href="category.html">Health Food</a></li>
-                            <li class="cat-item cat-item-5"><a class="text-white" href="category.html">Sleep Disorders</a></li>
-                        </ul>
-                    </div>
+                    @forelse($allCategories as $mainMenu)
+                        @php $descendants = $mainMenu->descendants; @endphp
+                        <div class="col">
+                            <h5 class="widget-title-muted mb-15"><a href="{{ route('category.posts', $mainMenu->slug) }}">{{ $mainMenu->name }}</a></h5>
+                            @if( count($descendants) > 0 )
+                                <ul class="mb-30 mr-30 text-white">
+                                @foreach($descendants as $subMenu)
+                                    <li class="cat-item cat-item-2"><a class="text-white" href="{{ route('category.posts', $subMenu->slug) }}">{{ $subMenu->name }}</a></li>
+                                @endforeach
+                                </ul>
+                            @endif
+                        </div>
+                    @empty
+                    @endforelse
                 </div>
             </div>
         </div>
